1af723d8905c9a703f2213fbd78e248db55205d6
swapnilsrivastava68 <56465793+swapnilsrivastava68@users.noreply.github.com>
2019-10-12 16:54:24 +0530

    changing login component

--- Frontend/bankApplication/src/app/component/login/login.component.ts
+++ Frontend/bankApplication/src/app/component/login/login.component.ts
@@ -13,18 +13,12 @@ export class LoginComponent {
     map(({ matches }) => {
       if (matches) {
         return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
+          { title: 'Login', cols: 1, rows: 1 },
         ];
       }
 
       return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
+        { title: 'Login', cols: 2, rows: 1 },
       ];
     })
   );
